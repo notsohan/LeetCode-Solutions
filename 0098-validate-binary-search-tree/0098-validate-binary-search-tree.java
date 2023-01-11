daff252117f42739f6338f796ec0d9cd0fa6682b
Sohanoor Rahman <52781863+notsohan@users.noreply.github.com>
2023-01-11 15:47:34 +0600

    Time: 0 ms (100.00%), Space: 41.9 MB (81.78%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,19 +15,30 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if(root==null){
+    //     if(root==null){
+    //         return true;
+    //     } else return isSubBSTLesser(root.left, root.val)
+    //             && isSubBSTGreater(root.right, root.val)
+    //             && isValidBST(root.left)
+    //             && isValidBST(root.right);
+    // }
+    // public static boolean isSubBSTLesser(TreeNode root, int val){
+    //     if(root==null) return true;
+    //     return root.val < val && isSubBSTLesser(root.left, val) && isSubBSTLesser(root.right, val);
+    // }
+    // public static boolean isSubBSTGreater(TreeNode root, int val){
+    //     if(root==null) return true;
+    //     return root.val > val && isSubBSTGreater(root.right, val) && isSubBSTGreater(root.left, val);
+        return isBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    public boolean isBST(TreeNode root, long minValue, long maxValue){
+        if(root==null) return true;
+        if(root.val>minValue && root.val<maxValue 
+           && isBST(root.left, minValue, root.val) 
+           && isBST(root.right, root.val, maxValue)){
             return true;
-        } else return isSubBSTLesser(root.left, root.val)
-                && isSubBSTGreater(root.right, root.val)
-                && isValidBST(root.left)
-                && isValidBST(root.right);
-    }
-    public static boolean isSubBSTLesser(TreeNode root, int val){
-        if(root==null) return true;
-        return root.val < val && isSubBSTLesser(root.left, val) && isSubBSTLesser(root.right, val);
-    }
-    public static boolean isSubBSTGreater(TreeNode root, int val){
-        if(root==null) return true;
-        return root.val > val && isSubBSTGreater(root.right, val) && isSubBSTGreater(root.left, val);
+        }else{
+            return false;
+        }
     }
 }
