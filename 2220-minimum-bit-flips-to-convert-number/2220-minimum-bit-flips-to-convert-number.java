68e773b699fb1c1a6b6fd22b543d99d60b64f46e
Sohanoor Rahman <52781863+notsohan@users.noreply.github.com>
2023-01-27 09:41:26 +0600

    Time: 1 ms (45.57%), Space: 41.3 MB (17.05%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,5 +1,11 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        return Integer.bitCount(start^goal);
+        //return Integer.bitCount(start^goal);
+        int flip = start^goal, count = 0;
+        while(flip>0){
+            count+=(flip&1);
+            flip = flip>>1;
+        }
+        return count;
     }
 }
