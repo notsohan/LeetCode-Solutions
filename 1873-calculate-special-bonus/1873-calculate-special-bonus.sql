fc41a7a183c71faea2f84c9fa73a14787563b910
Sohanoor Rahman <52781863+notsohan@users.noreply.github.com>
2023-02-09 20:02:42 +0600

    Time: 1183 ms (28.83%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT employee_id, salary AS bonus
+FROM Employees
+WHERE employee_id%2<>0 AND name NOT LIKE 'M%'
+UNION
+SELECT employee_id, 0 AS bonus
+FROM Employees
+WHERE employee_id%2=0 or name LIKE 'M%'
+ORDER BY employee_id;
