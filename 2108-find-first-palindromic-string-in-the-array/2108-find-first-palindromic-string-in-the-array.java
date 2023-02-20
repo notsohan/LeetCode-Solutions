46600e136051f847f698f961bf80c417ec99efd1
Sohanoor Rahman <52781863+notsohan@users.noreply.github.com>
2023-02-21 01:43:57 +0600

    Time: 2 ms (99.68%), Space: 42.3 MB (84.42%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        String s = "";
+        for (String word : words) {
+            if (isPalin(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+    public boolean isPalin(String word){
+        for(int i=0, j=word.length()-1; i<word.length(); i++, j--){
+            if(word.charAt(i)!=word.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
